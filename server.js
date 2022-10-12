ac32015e99d331e75a79a949154ee90c3d9c546e
Musabii Mambetov <musabi@live.com>
2022-10-12 12:35:16 +0300

    add timestamp

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const socketio = require("socket.io");
 const cors = require("cors");
 const utilUsers = require("./utils/users");
+const moment = require('moment');
 
 const app = express();
 app.use(cors);
@@ -18,32 +19,47 @@ const io = socketio(server, {
 io.on("connection", (socket) => {
   console.log("connect new");
 
+  socket.emit("message", {
+    username: "Bot",
+    text: `you are Welcome!`,
+    timestamp:moment().format('LTS')
+  });
+
   socket.on("join", (user) => {
     utilUsers.addUser(socket.id, user.username, user.chatName);
 
     socket.join(user.chatName);
-    io.to(user.chatName).emit("updateUsers",utilUsers.getUsers(user.chatName))
+    io.to(user.chatName).emit("updateUsers", utilUsers.getUsers(user.chatName));
+
     socket.broadcast.to(user.chatName).emit("message", {
       username: "Bot",
       text: `${user.username} join to chat`,
+      timestamp:moment().format('LTS')
     });
   });
 
   socket.on("chatMessage", (message) => {
     const user = utilUsers.getUser(socket.id);
     if (user) {
+      // const timestamp = moment().calendar();
+      message.timestamp = moment().format('LTS');
       console.log({ message });
       io.to(user.chatName).emit("message", message);
     }
   });
 
   socket.on("disconnect", (e) => {
+    console.log("disconnect");
     const user = utilUsers.removeUser(socket.id);
     if (user) {
-      io.to(user.chatName).emit("updateUsers",utilUsers.getUsers(user.chatName))
+      io.to(user.chatName).emit(
+        "updateUsers",
+        utilUsers.getUsers(user.chatName)
+      );
       io.to(user.chatName).emit("message", {
         username: "Bot",
         text: `${user.username} left the chat`,
+      timestamp:moment().format('LTS')
       });
     }
   });
